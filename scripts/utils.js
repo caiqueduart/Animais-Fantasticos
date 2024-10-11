f6e98fa2b70def39ff300dbc40b0f48ab1463960
caiqueduart <dcaique242@gmail.com>
2024-10-10 21:17:08 -0300

    Estudando...

--- scripts/utils.js
+++ scripts/utils.js
@@ -1,14 +1,21 @@
-function activateSlides() {
+function contentChange() {
     const images = document.querySelectorAll('.animais-lista li');
+    const descriptions = document.querySelectorAll('.animais-descricao section');
+    const classSelected = 'selected';
+    const classShowDescription = 'showDescription';
     
     images.forEach((image, index, array) => {
         image.addEventListener('click', (event) => {
     
             array.forEach((restImage) => {
-                restImage.classList.remove('selected');
+                restImage.classList.remove(classSelected);
             });
-    
-            event.currentTarget.classList.add('selected');
+            event.currentTarget.classList.add(classSelected);
+
+            descriptions.forEach( description => {
+                description.classList.remove(classShowDescription);
+            });
+            descriptions[index].classList.add(classShowDescription);
         });
     });
 }
@@ -18,16 +25,32 @@ function activateNav() {
     
     menuLinks.forEach(link => {
         link.addEventListener('click', event => {
-            menuLinks.forEach(link => {
-                link.classList.remove('activate');
-            });
+            event.preventDefault();
 
-            event.currentTarget.classList.add('activate');
+            const section = document.querySelector(
+                event.currentTarget.getAttribute('href')
+            );
+            
+            section.scrollIntoView({
+                behavior: 'smooth',
+                block: 'start',
+            });
         });
     });
+}
 
+function showQuention() {
+    const questions = document.querySelectorAll('.faq-lista dt');
+    const classShowQuestion = 'showQuestion';
     
+    questions.forEach(question => {
+        question.addEventListener('click', event => {
+            event.currentTarget.classList.toggle(classShowQuestion);
+            event.currentTarget.nextElementSibling.classList.toggle(classShowQuestion);
+        });
+    })
 }
 
 activateNav();
-activateSlides();
+contentChange();
+showQuention();
